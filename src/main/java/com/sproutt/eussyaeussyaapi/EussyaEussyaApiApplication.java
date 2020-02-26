48a57911bfcc3e9b3da14ff912336cb3cb1d8a06
Bellroute <kjkun7631@gmail.com>
2020-02-27 01:07:20 +0900

    setting: spring boot preferences

---
+++ src/main/java/com/sproutt/eussyaeussyaapi/EussyaEussyaApiApplication.java
@@ -0,0 +1,13 @@
+package com.sproutt.eussyaeussyaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EussyaEussyaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EussyaEussyaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sproutt/eussyaeussyaapi/EussyaEussyaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sproutt.eussyaeussyaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EussyaEussyaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
