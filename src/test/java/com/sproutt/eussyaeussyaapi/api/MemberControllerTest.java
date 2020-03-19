8a138f0305595af0cba7bf5332d1e4be7d4e9c2b
Bellroute <kjkun7631@gmail.com>
2020-03-19 22:11:13 +0900

    chore: resolve conflict

--- src/test/java/com/sproutt/eussyaeussyaapi/api/MemberControllerTest.java
+++ src/test/java/com/sproutt/eussyaeussyaapi/api/MemberControllerTest.java
@@ -2,23 +2,15 @@ package com.sproutt.eussyaeussyaapi.api;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-<<<<<<< HEAD
 import com.sproutt.eussyaeussyaapi.api.dto.EmailDTO;
-import com.sproutt.eussyaeussyaapi.api.dto.JoinDTO;
-import com.sproutt.eussyaeussyaapi.application.JwtService;
-import com.sproutt.eussyaeussyaapi.application.MailService;
-import com.sproutt.eussyaeussyaapi.application.MemberService;
-import com.sproutt.eussyaeussyaapi.domain.Member;
-import com.sproutt.eussyaeussyaapi.domain.exceptions.DuplicationMemberException;
-import com.sproutt.eussyaeussyaapi.utils.RandomGenerator;
-=======
 import com.sproutt.eussyaeussyaapi.api.member.MemberController;
-import com.sproutt.eussyaeussyaapi.api.security.JwtService;
-import com.sproutt.eussyaeussyaapi.domain.member.exceptions.DuplicatedMemberIdException;
-import com.sproutt.eussyaeussyaapi.application.member.MemberService;
-import com.sproutt.eussyaeussyaapi.domain.member.Member;
 import com.sproutt.eussyaeussyaapi.api.member.dto.JoinDTO;
->>>>>>> b30e337debff478895312df591fef1938492967e
+import com.sproutt.eussyaeussyaapi.api.security.JwtService;
+import com.sproutt.eussyaeussyaapi.application.MailService;
+import com.sproutt.eussyaeussyaapi.application.member.MemberService;
+import com.sproutt.eussyaeussyaapi.domain.exceptions.DuplicationMemberException;
+import com.sproutt.eussyaeussyaapi.domain.member.Member;
+import com.sproutt.eussyaeussyaapi.utils.RandomGenerator;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
