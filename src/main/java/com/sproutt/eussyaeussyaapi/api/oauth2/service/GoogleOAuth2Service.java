a389bfe4bbdfa4157d95b1759a2c4a0d14d4e91e
jbj616 <jbj616@gmail.com>
2020-03-22 14:11:33 +0900

    add : google oauth 를 활용한 로그인, 가입 기능

---
+++ src/main/java/com/sproutt/eussyaeussyaapi/api/oauth2/service/GoogleOAuth2Service.java
@@ -0,0 +1,63 @@
+package com.sproutt.eussyaeussyaapi.api.oauth2.service;
+
+import com.sproutt.eussyaeussyaapi.api.oauth2.dto.GithubOAuth2UserDto;
+import com.sproutt.eussyaeussyaapi.api.oauth2.dto.GoogleOAuth2UserDto;
+import com.sproutt.eussyaeussyaapi.domain.member.Member;
+import com.sproutt.eussyaeussyaapi.domain.member.MemberRepository;
+import com.sproutt.eussyaeussyaapi.domain.member.exceptions.DuplicationMemberException;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@RequiredArgsConstructor
+@Service
+public class GoogleOAuth2Service{
+
+    private final RestTemplate restTemplate;
+    private final MemberRepository memberRepository;
+
+    @Value("${social.github.url}")
+    private String requestUrl;
+
+    private GoogleOAuth2UserDto getGithubUserInfo(String accessToken) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + accessToken);
+
+        HttpEntity request = new HttpEntity(headers);
+        try {
+            ResponseEntity<GoogleOAuth2UserDto> response = restTemplate
+                .exchange(requestUrl, HttpMethod.GET, request, GoogleOAuth2UserDto.class);
+
+            return response.getBody();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        throw new RuntimeException();
+    }
+
+    public Member getMemberInfo(String accessToken) {
+        GoogleOAuth2UserDto googleOAuth2UserDto = getGithubUserInfo(accessToken);
+
+        return memberRepository.findByMemberId(googleOAuth2UserDto.getId()).orElseThrow(RuntimeException::new);
+    }
+
+    public Member createMember(String accessToken) {
+        GoogleOAuth2UserDto githubOAuth2User = getGithubUserInfo(accessToken);
+
+        Optional<Member> member = memberRepository.findByMemberId(githubOAuth2User.getId());
+
+        if (member.isPresent()) {
+            throw new DuplicationMemberException();
+        }
+
+        return memberRepository.save(githubOAuth2User.toEntity());
+    }
+}
