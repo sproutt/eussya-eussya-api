4ffbe81a1dd1ed2ae127503534e6ed6e6786e269
jbj616 <jbj616@gmail.com>
2020-03-23 00:05:55 +0900

    add : oauth2 관련 test 코드 추가

---
+++ src/test/java/com/sproutt/eussyaeussyaapi/acceptance/SocialAcceptanceTest.java
@@ -0,0 +1,68 @@
+package com.sproutt.eussyaeussyaapi.acceptance;
+
+import com.sproutt.eussyaeussyaapi.domain.member.Member;
+import com.sproutt.eussyaeussyaapi.domain.member.MemberRepository;
+import com.sproutt.eussyaeussyaapi.utils.ExceptionMessage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class SocialAcceptanceTest {
+
+    private static final String GITHUB_ACCESS_TOKEN = "9af3d28fa95a4c086314c1aec2c0d733f7f4bd3c";
+
+    @Autowired
+    private TestRestTemplate template;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @BeforeEach
+    public void init() {
+        memberRepository.deleteAll();
+        memberRepository.flush();
+    }
+
+    @Test
+    public void create_member_by_github() {
+        HttpHeaders headers = new HttpHeaders();
+        ResponseEntity response = template.postForEntity("/social/signup/github", getHeader(GITHUB_ACCESS_TOKEN), Void.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+    }
+
+    @Test
+    public void create_member_by_github_but_already_exist_memberId(){
+        memberRepository.save(githubMember());
+
+        ResponseEntity response = template.postForEntity("/social/signup/github", getHeader(GITHUB_ACCESS_TOKEN), String.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+        assertThat(response.getBody().toString()).contains(ExceptionMessage.DUPLICATED_MEMBER_ID);
+    }
+
+    private HttpHeaders getHeader(String accessToken){
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("accessToken", accessToken);
+
+        return headers;
+    }
+
+    private Member githubMember() {
+        return Member.builder()
+            .memberId("41421173")
+            .build();
+    }
+}
---
+++ src/test/java/com/sproutt/eussyaeussyaapi/application/GithubOAuth2ServiceTest.java
@@ -0,0 +1,77 @@
+package com.sproutt.eussyaeussyaapi.application;
+
+import com.sproutt.eussyaeussyaapi.api.oauth2.service.GithubOAuth2Service;
+import com.sproutt.eussyaeussyaapi.domain.member.Member;
+import com.sproutt.eussyaeussyaapi.domain.member.MemberRepository;
+import com.sproutt.eussyaeussyaapi.domain.member.exceptions.DuplicationMemberException;
+import com.sproutt.eussyaeussyaapi.domain.member.exceptions.NoSuchMemberException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.web.client.RestTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class GithubOAuth2ServiceTest {
+
+    private static final String GITHUB_ACCESS_TOKEN = "9af3d28fa95a4c086314c1aec2c0d733f7f4bd3c";
+
+    @Autowired
+    private GithubOAuth2Service githubOAuth2Service;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @BeforeEach
+    public void before() {
+        memberRepository.deleteAll();
+        memberRepository.flush();
+    }
+
+    @Test
+    public void create_member_with_Github() {
+        Member member = githubOAuth2Service.createMember(GITHUB_ACCESS_TOKEN);
+        Member createdMember = memberRepository.findAll().get(0);
+
+        assertThat(createdMember.getMemberId()).isEqualTo(member.getMemberId());
+        assertThat(createdMember.getNickName()).isEqualTo(member.getNickName());
+    }
+
+    @Test
+    public void create_member_with_Github_이미_가입된() {
+        Member githubMember = githubMember();
+        memberRepository.save(githubMember);
+
+        assertThrows(DuplicationMemberException.class, () -> githubOAuth2Service.createMember(GITHUB_ACCESS_TOKEN));
+    }
+
+    @Test
+    public void getMemberInfo() {
+        Member githubMember = githubMember();
+        memberRepository.save(githubMember);
+
+        Member savedMember = githubOAuth2Service.getMemberInfo(GITHUB_ACCESS_TOKEN);
+
+        assertThat(savedMember.getMemberId()).isEqualTo(githubMember.getMemberId());
+        assertThat(savedMember.getNickName()).isEqualTo(githubMember.getNickName());
+    }
+
+    @Test
+    public void getMemberInfo_저장되지않은() {
+        assertThrows(NoSuchMemberException.class, () -> githubOAuth2Service.getMemberInfo(GITHUB_ACCESS_TOKEN));
+    }
+
+    private Member githubMember() {
+        return Member.builder()
+            .memberId("41421173")
+            .nickName("Byeongjae Jung")
+            .build();
+    }
+
+}
