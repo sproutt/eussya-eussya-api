44d41dfa14690a95447eb69da4a548669c2aaabd
jbj616 <jbj616@gmail.com>
2020-03-27 19:02:19 +0900

    add : refactorying test Code after changing jasypt

--- src/test/java/com/sproutt/eussyaeussyaapi/application/GithubOAuth2ServiceTest.java
+++ src/test/java/com/sproutt/eussyaeussyaapi/application/GithubOAuth2ServiceTest.java
@@ -1,29 +1,27 @@
 package com.sproutt.eussyaeussyaapi.application;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import com.sproutt.eussyaeussyaapi.api.oauth2.service.GithubOAuth2Service;
 import com.sproutt.eussyaeussyaapi.domain.member.Member;
 import com.sproutt.eussyaeussyaapi.domain.member.MemberRepository;
-import com.sproutt.eussyaeussyaapi.domain.member.Provider;
 import com.sproutt.eussyaeussyaapi.domain.member.exceptions.DuplicationMemberException;
 import com.sproutt.eussyaeussyaapi.domain.member.exceptions.NoSuchMemberException;
+import com.sproutt.eussyaeussyaapi.object.EncryptedResourceGenerator;
 import com.sproutt.eussyaeussyaapi.object.MemberFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = "classpath:/application.yml")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class GithubOAuth2ServiceTest {
 
-    @Value("${social.github.token}")
-    private String GITHUB_ACCESS_TOKEN;
+    private String githubToken = EncryptedResourceGenerator.getGitToken();
 
     @Autowired
     private GithubOAuth2Service githubOAuth2Service;
@@ -39,7 +37,7 @@ public class GithubOAuth2ServiceTest {
 
     @Test
     public void create_member_with_Github() {
-        Member member = githubOAuth2Service.createMember(GITHUB_ACCESS_TOKEN);
+        Member member = githubOAuth2Service.createMember(githubToken);
         Member createdMember = memberRepository.findAll().get(0);
 
         assertThat(createdMember.getMemberId()).isEqualTo(member.getMemberId());
@@ -51,7 +49,7 @@ public class GithubOAuth2ServiceTest {
         Member githubMember = MemberFactory.getGithubMember();
         memberRepository.save(githubMember);
 
-        assertThrows(DuplicationMemberException.class, () -> githubOAuth2Service.createMember(GITHUB_ACCESS_TOKEN));
+        assertThrows(DuplicationMemberException.class, () -> githubOAuth2Service.createMember(githubToken));
     }
 
     @Test
@@ -59,7 +57,7 @@ public class GithubOAuth2ServiceTest {
         Member githubMember = MemberFactory.getGithubMember();
         memberRepository.save(githubMember);
 
-        Member savedMember = githubOAuth2Service.getMemberInfo(GITHUB_ACCESS_TOKEN);
+        Member savedMember = githubOAuth2Service.getMemberInfo(githubToken);
 
         assertThat(savedMember.getMemberId()).isEqualTo(githubMember.getMemberId());
         assertThat(savedMember.getNickName()).isEqualTo(githubMember.getNickName());
@@ -67,6 +65,6 @@ public class GithubOAuth2ServiceTest {
 
     @Test
     public void getMemberInfo_저장되지않은() {
-        assertThrows(NoSuchMemberException.class, () -> githubOAuth2Service.getMemberInfo(GITHUB_ACCESS_TOKEN));
+        assertThrows(NoSuchMemberException.class, () -> githubOAuth2Service.getMemberInfo(githubToken));
     }
 }
