d8e7cf1a1e53bfc22a7c3f220e17ca893bb4bc3c
jbj616 <jbj616@gmail.com>
2020-03-27 19:01:42 +0900

    add : JasyptConfig

--- src/main/java/com/sproutt/eussyaeussyaapi/EussyaEussyaApiApplication.java
+++ src/main/java/com/sproutt/eussyaeussyaapi/EussyaEussyaApiApplication.java
@@ -1,5 +1,6 @@
 package com.sproutt.eussyaeussyaapi;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -10,8 +11,9 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication
+@EnableEncryptableProperties
 @EnableSwagger2
+@SpringBootApplication
 public class EussyaEussyaApiApplication {
 
     public static void main(String[] args) {
@@ -19,16 +21,17 @@ public class EussyaEussyaApiApplication {
     }
 
     @Bean
-    public RestTemplate getRestTemplate(){
+    public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
 
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.sproutt.eussyaeussyaapi"))
-                .paths(PathSelectors.any())
-                .build();
+            .select()
+            .apis(RequestHandlerSelectors.basePackage("com.sproutt.eussyaeussyaapi"))
+            .paths(PathSelectors.any())
+            .build();
     }
 }
+
