d560aecabfa5121fd25e92a1bd2f980ea65c3b78
jbj616 <jbj616@gmail.com>
2020-04-16 13:42:26 +0900

    refactor : response error 추가

--- src/main/java/com/sproutt/eussyaeussyaapi/api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sproutt/eussyaeussyaapi/api/exception/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import com.sproutt.eussyaeussyaapi.domain.member.exceptions.DuplicationException
 import com.sproutt.eussyaeussyaapi.domain.member.exceptions.NoSuchMemberException;
 import com.sproutt.eussyaeussyaapi.domain.member.exceptions.VerificationException;
 import com.sproutt.eussyaeussyaapi.domain.member.exceptions.WrongPasswordException;
+import com.sproutt.eussyaeussyaapi.utils.Result;
 import javassist.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -47,64 +48,64 @@ public class GlobalExceptionHandler {
 
             response.addValidateError(new ValidateError(fieldError.getField(), getErrorMessage(fieldError)));
 
-            log.info("error code: {}", getFirstCode(fieldError));
-            log.info("error arguments: {}", fieldError.getArguments());
+            log.info("badRequest code: {}", getFirstCode(fieldError));
+            log.info("badRequest arguments: {}", fieldError.getArguments());
             log.info("default message: {}", fieldError.getDefaultMessage());
             log.info("changed message: {}", getErrorMessage(fieldError));
         }
 
         log.info("response: {}", response.toString());
 
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        return Result.badRequest(response);
     }
 
     @ExceptionHandler(value = DuplicationException.class)
     public ResponseEntity handleDuplicationException(DuplicationException exception) {
         log.info("handleDuplicationException : {}", exception);
 
-        return ResponseEntity.badRequest().contentType(MediaType.APPLICATION_JSON).body(exception.getMessage());
+        return Result.badRequest(exception.getMessage());
     }
 
     @ExceptionHandler(value = WrongPasswordException.class)
     public ResponseEntity handleWrongPasswordException(WrongPasswordException exception) {
         log.info("handleWrongPasswordException : {}", exception);
 
-        return ResponseEntity.badRequest().contentType(MediaType.APPLICATION_JSON).body(exception.getMessage());
+        return Result.badRequest(exception.getMessage());
     }
 
     @ExceptionHandler(value = VerificationException.class)
     public ResponseEntity handleVerificationException(VerificationException exception) {
         log.info("handleVerificationException : {}", exception);
 
-        return ResponseEntity.badRequest().contentType(MediaType.APPLICATION_JSON).body(exception.getMessage());
+        return Result.badRequest(exception.getMessage());
     }
 
     @ExceptionHandler(value = NotFoundException.class)
     public ResponseEntity handleNotFoundException(NotFoundException exception) {
         log.info("handleNotFoundException : {}", exception);
 
-        return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(exception.getMessage());
+        return Result.notFound(exception.getMessage());
     }
 
     @ExceptionHandler(value = NoSuchMemberException.class)
     public ResponseEntity handleNoSuchMemberException(NoSuchMemberException exception) {
         log.info("handleNoSuchMemberException : {}", exception);
 
-        return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(exception.getMessage());
+        return Result.notFound(exception.getMessage());
     }
 
     @ExceptionHandler(value = OAuth2CommunicationException.class)
     public ResponseEntity handleOAuth2CommunicationException(OAuth2CommunicationException exception) {
         log.info("handleOAuth2CommunicationException : {}", exception);
 
-        return ResponseEntity.badRequest().contentType(MediaType.APPLICATION_JSON).body(exception.getMessage());
+        return Result.badRequest(exception.getMessage());
     }
 
     @ExceptionHandler(value = UnSupportedOAuth2Exception.class)
     public ResponseEntity handleUnSupportOAuth2Exception(UnSupportedOAuth2Exception exception) {
         log.info("handleUnSupportedOAuth2Exception : {}", exception);
 
-        return ResponseEntity.badRequest().contentType(MediaType.APPLICATION_JSON).body(exception.getMessage());
+        return Result.badRequest(exception.getMessage());
     }
 
     private String getErrorMessage(FieldError fieldError) {
--- src/main/java/com/sproutt/eussyaeussyaapi/utils/Result.java
+++ src/main/java/com/sproutt/eussyaeussyaapi/utils/Result.java
@@ -7,29 +7,39 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
 @NoArgsConstructor
-public class Result<T> {
+public class Result {
 
-    public static <T> ResponseEntity<T> created() {
+    public static ResponseEntity created() {
 
-        return new ResponseEntity<T>(getHeaders(), HttpStatus.CREATED);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                             .contentType(MediaType.APPLICATION_JSON)
+                             .build();
     }
 
-    public static <T> ResponseEntity<T> ok() {
+    public static ResponseEntity ok() {
 
-        return new ResponseEntity<T>(getHeaders(), HttpStatus.OK);
+        return ResponseEntity.ok()
+                             .contentType(MediaType.APPLICATION_JSON)
+                             .build();
     }
 
-    public static <T> ResponseEntity<T> okWithToken(String tokenKey, String token) {
-        HttpHeaders headers = getHeaders();
-        headers.set(tokenKey, token);
-
-        return new ResponseEntity<T>(headers, HttpStatus.OK);
-    }
-
-    private static HttpHeaders getHeaders() {
+    public static ResponseEntity okWithToken(String tokenKey, String token) {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set(tokenKey, token);
 
-        return headers;
+        return new ResponseEntity(headers, HttpStatus.OK);
+    }
+
+    public static <T> ResponseEntity<T> badRequest(T message) {
+        return ResponseEntity.badRequest()
+                             .contentType(MediaType.APPLICATION_JSON)
+                             .body(message);
+    }
+
+    public static <T> ResponseEntity<T> notFound(T message) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND.value())
+                             .contentType(MediaType.APPLICATION_JSON)
+                             .body(message);
     }
 }
