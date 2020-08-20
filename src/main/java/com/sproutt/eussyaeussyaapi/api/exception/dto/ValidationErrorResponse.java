dfe54779e4bb31a0a26bc0c87a5c7eb163b4d855
Bellroute <kjkun7631@gmail.com>
2020-08-20 14:08:24 +0900

    fix: test 코드

--- src/main/java/com/sproutt/eussyaeussyaapi/api/exception/dto/ValidationErrorResponse.java
+++ src/main/java/com/sproutt/eussyaeussyaapi/api/exception/dto/ValidationErrorResponse.java
@@ -1,8 +1,12 @@
 package com.sproutt.eussyaeussyaapi.api.exception.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Getter
 public class ValidationErrorResponse extends ErrorResponse {
     private List<ValidateError> errors;
 
@@ -17,8 +21,13 @@ public class ValidationErrorResponse extends ErrorResponse {
     @Override
     public String toString() {
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("ErrorResponse{")
-                     .append("errors={");
+        stringBuilder.append("ValidationErrorResponse{")
+                     .append("ErrorResponse{")
+                     .append("code=")
+                     .append(getCode())
+                     .append(", message=")
+                     .append(getMessage())
+                     .append("}, errors={");
 
         errors.forEach(error -> stringBuilder.append(error.toString()).append(", "));
         stringBuilder.replace(stringBuilder.length() - 2, stringBuilder.length(), "");
