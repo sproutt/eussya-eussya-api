ab9c937d88d1b78ab90bc976b04394ac5a25022b
Bellroute <kjkun7631@gmail.com>
2020-08-22 23:16:43 +0900

    feat: 미션 결과물 관련 api 추가

--- src/main/java/com/sproutt/eussyaeussyaapi/utils/ExceptionMessage.java
+++ src/main/java/com/sproutt/eussyaeussyaapi/utils/ExceptionMessage.java
@@ -16,4 +16,5 @@ public class ExceptionMessage {
     public static final String NOT_SATISFIED_CONDITION = "목표 시간을 채우지 못했습니다.";
     public static final String EXPIRED_MISSION = "종료된 미션입니다.";
     public static final String UN_AUTHENTICATED_EMAIL = "인증되지 않은 email입니다.";
+    public static final String NOT_COMPLETED_MISSION = "완료하지 않은 미션은 결과물을 작성할 수 없습니다.";
 }
