864a2897dc03d4e04a0213300299386e8591bdec
UpperLeaf <tkdduq8260@gmail.com>
2020-09-02 09:49:20 +0900

    refactor : 인증코드 생성 방식 변경

--- src/main/java/com/sproutt/eussyaeussyaapi/utils/RandomGenerator.java
+++ src/main/java/com/sproutt/eussyaeussyaapi/utils/RandomGenerator.java
@@ -1,21 +1,9 @@
 package com.sproutt.eussyaeussyaapi.utils;
 
-import java.util.Random;
+import java.util.UUID;
 
 public class RandomGenerator {
-    private static final int AUTHENTICATION_CODE_LENGTH = 8;
-    private static final char[] CHARSET = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
-            'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
-            'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
-
     public static String createAuthenticationCode() {
-        StringBuilder code = new StringBuilder();
-        Random random = new Random(System.currentTimeMillis());
-
-        while (code.length() != AUTHENTICATION_CODE_LENGTH) {
-            code.append(CHARSET[random.nextInt(CHARSET.length)]);
-        }
-
-        return code.toString();
+        return UUID.randomUUID().toString();
     }
 }
