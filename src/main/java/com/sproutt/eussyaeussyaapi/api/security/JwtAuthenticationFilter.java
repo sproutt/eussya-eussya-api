d9a1a54ea5bd7f2cd662570722144c3ed1161afd
Bellroute <kjkun7631@gmail.com>
2021-01-21 17:51:01 +0900

    hotfix: cors 이슈 해결 - websecurity로 ignoring 되고 있던 "/login", "/members/**" 를 해당 설정에서 제외 시킴(시큐리티 설정이 적용이 되지 않아서 options 요청이 막혔었음) - application.yml에 authorizedRedirectUrl 변경 - JwtAuthenticatinFilter의 부모 클래스 OncePerRequestFilter로 변경

--- src/main/java/com/sproutt/eussyaeussyaapi/api/security/JwtAuthenticationFilter.java
+++ src/main/java/com/sproutt/eussyaeussyaapi/api/security/JwtAuthenticationFilter.java
@@ -2,16 +2,15 @@ package com.sproutt.eussyaeussyaapi.api.security;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class JwtAuthenticationFilter extends GenericFilterBean {
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtHelper jwtHelper;
 
@@ -20,8 +19,8 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
     }
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        String token = ((HttpServletRequest) request).getHeader(JwtHelper.ACCESS_TOKEN_HEADER);
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        String token = request.getHeader(JwtHelper.ACCESS_TOKEN_HEADER);
 
         if (token != null && jwtHelper.isUsable(token)) {
             Authentication authentication = jwtHelper.getAuthentication(token);
@@ -29,7 +28,7 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
                                  .setAuthentication(authentication);
         }
 
-        chain.doFilter(request, response);
+        filterChain.doFilter(request, response);
     }
 }
 
