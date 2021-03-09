d397db8fc47e5e0a8a8d34f2d2010597116169cc
Ilisha29 <94konzern@gmail.com>
2021-03-09 21:15:27 +0900

    feat : Issue140 - Image Upload && Image URL get

--- src/main/java/com/sproutt/eussyaeussyaapi/application/file/FileService.java
+++ src/main/java/com/sproutt/eussyaeussyaapi/application/file/FileService.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 
 @Service
 public interface FileService {
+
     Optional<File> findByNickName(String nickName);
 
     void saveProfile(Member loginMember, String storagePath);
